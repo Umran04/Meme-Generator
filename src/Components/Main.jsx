d678c90ad351a4561408ec9e7b776dc5298a0171
Umran <mohammedumran.0416@gmail.com>
2025-09-26 18:17:47 +0100

    made it so that the toptext input values change on the ui itself

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,4 +1,25 @@
+import { useState } from "react"
+
 export default function Main() {
+
+    const [meme,setMeme] = useState({
+        imageURL : "http://i.imgflip.com/1bij.jpg",
+        topText : 'One does not simply',
+        bottomText :  'Walk into Mordor'
+    })
+
+
+
+    function handleChange(event){
+        const {value} = event.currentTarget
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            topText:value
+        }))
+    }
+
+
+
     return (
         <main>
             <div className="form">
@@ -7,6 +28,8 @@ export default function Main() {
                         type="text"
                         placeholder="One does not simply"
                         name="topText"
+                        onChange={handleChange}
+                        
                     />
                 </label>
 
@@ -20,9 +43,9 @@ export default function Main() {
                 <button>Get a new meme image 🖼</button>
             </div>
             <div className="meme">
-                <img src="http://i.imgflip.com/1bij.jpg" />
-                <span className="top">One does not simply</span>
-                <span className="bottom">Walk into Mordor</span>
+                <img src={meme.imageURL} />
+                <span className="top">{meme.topText}</span>
+                <span className="bottom">{meme.bottomText}</span>
             </div>
         </main>
     )
