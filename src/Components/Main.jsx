aa31fe4498e8a2f7e3b2cedd878af4ec3f1deefc
Umran <mohammedumran.0416@gmail.com>
2025-09-27 07:52:39 +0100

    can change top and bottom texts to match respectively

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -11,10 +11,10 @@ export default function Main() {
 
 
     function handleChange(event){
-        const {value} = event.currentTarget
+        const {value, name} = event.currentTarget
         setMeme(prevMeme => ({
             ...prevMeme,
-            topText:value
+            [name]:value
         }))
     }
 
@@ -28,8 +28,8 @@ export default function Main() {
                         type="text"
                         placeholder="One does not simply"
                         name="topText"
-                        onChange={handleChange}
-                        
+                        onChange={handleChange}   
+                        value={meme.topText}
                     />
                 </label>
 
@@ -38,6 +38,8 @@ export default function Main() {
                         type="text"
                         placeholder="Walk into Mordor"
                         name="bottomText"
+                        onChange={handleChange}   
+                        value={meme.bottomText}
                     />
                 </label>
                 <button>Get a new meme image 🖼</button>
